40c55e24b868425053aaf065bc439772b2db14e8
ananthkrishnan kr <95912110+hiananth@users.noreply.github.com>
2022-02-07 11:32:58 +0530

    Add files via upload

---
+++ ARRAY.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+
+ int a[5],n,i;
+   clrscr();
+ printf("enter the limit");
+ scanf("%d",&n);
+ printf("enter the elements");
+ for(i=0;i<n;i++);
+ {
+    scanf("%d",&a[i]);
+    }
+   printf("print the elements");
+   printf("%d",a[i]);
+}
+getch();
+
+
+
